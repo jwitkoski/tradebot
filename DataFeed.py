2d3f1d41dff1e3c10afdd570afab93e00b3a14d6
Jim Witkoski <jwitkoski@localhost.localdomain>
2018-03-08 19:52:22 -0500

    DataFeed.py contains proof-of-concept; calling the constructor with 'wss://ws-feed.gdax.com' as the argument will result in some kind of communication from GDAX.  I dont know what exactly it prints, but it definitely is not random

--- DataFeed.py
+++ DataFeed.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 '''
 GDAX Web socket for live feed:
 wss://ws-feed.gdax.com
@@ -7,16 +8,45 @@ use that to sub to the feed above^
 
 Python also has a JSON encoder and decoder that we will use to pack/parse messages
 '''
+
 import websocket as ws
 import json
         
 class DataFeed:
-    def __init__(self, addr, [pairs], [channels]):
+    def __init__(self, addr):
         self.addr = addr
-        self.endpoint = ws.WebSocket()
-        self.endpoint.connect(str(addr),...)
+        self.endpoint = ws.create_connection(str(addr))
+        self.subscribe()
+         
+    def subscribe(self):
+        sub_msg = json.dumps(\
+        {\
+            "type": "subscribe",\
+            "product_ids": [\
+                "ETH-USD",\
+            ],\
+            "channels": [\
+                "level2",\
+                "heartbeat",\
+                {\
+                    "name": "ticker",\
+                    "product_ids": [\
+                        "ETH-USD",\
+                    ]\
+                }\
+            ]\
+        })
+        self.endpoint.send(sub_msg)
+        self.recvMsg(self.endpoint.recv())
+
+    def recvMsg(self, msg):
+        self.response = json.loads(msg)
+        print msg
+        print 'Type: %s' % self.response['type']
+
         
     def listen(self):
+        self.endpoint.run_forever(sslopt={"check_hostname":False})
         '''
         - Runs on its own thread
         - Subscribe to desired pairs (ie. ETH/USDT) and channels
@@ -49,3 +79,4 @@ class DataFeed:
                     #func = getattr(self, recvd_msg['type'] + '_cb')
                     #thread(func, recvd_msg)
             -Callbacks will parse the rest of message and send relevant data off to TensorFlow to get learnt
+        '''
